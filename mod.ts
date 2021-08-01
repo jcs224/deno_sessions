9f570701afe5edf6f1bad301eb0bf7a0c1d37210
AEtheve <alexis97429.etheve@gmail.com>
2021-08-01 16:40:43 +0400

    add MongoDB store

--- mod.ts
+++ mod.ts
@@ -1,15 +1,17 @@
-import OakSession from './src/frameworks/OakSession.js'
-import OpineSession from './src/frameworks/OpineSession.js'
-import MemoryStore from './src/stores/MemoryStore.js'
-import SqliteStore from './src/stores/SqliteStore.js'
-import RedisStore from './src/stores/RedisStore.js'
-import WebdisStore from './src/stores/WebdisStore.js'
+import OakSession from "./src/frameworks/OakSession.js";
+import OpineSession from "./src/frameworks/OpineSession.js";
+import MemoryStore from "./src/stores/MemoryStore.js";
+import SqliteStore from "./src/stores/SqliteStore.js";
+import RedisStore from "./src/stores/RedisStore.js";
+import WebdisStore from "./src/stores/WebdisStore.js";
+import MongoStore from "./src/stores/MongoStore.js";
 
 export {
+  MemoryStore,
+  MongoStore,
   OakSession,
   OpineSession,
-  MemoryStore,
-  SqliteStore,
   RedisStore,
+  SqliteStore,
   WebdisStore,
-}
+};
